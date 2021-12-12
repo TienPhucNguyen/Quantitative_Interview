28d101e16fceac153ffab2bf8db76ad0e8755e2d
TienPhucNguyen <phucnt.ct@gmail.com>
2021-12-12 13:18:25 +0100

    Update question 2

--- question2/question2.py
+++ question2/question2.py
@@ -1,4 +1,10 @@
-# This question will be done later.
-def exists(v):
-    v = 10
-    return v
+def exists(v,
+          symbols=['α', 'β', 'γ', 'δ', 'ε', 'ζ', 'η', 'θ', 'ι', 'κ', 'λ', 'μ', 'ν', 'ξ', 'ο', 'π', 'ρ', 'σ/ς', 'τ', 'υ',
+                   'φ',
+                   'χ', 'ψ', 'ω']):
+    return v in symbols
+
+if exists('v'):
+    print('v has been defined')
+else:
+    print("v hasn't been defined")
