388af517fb2f5623990a4d278fb14e2b3ff3cbd5
TienPhucNguyen <phucnt.ct@gmail.com>
2021-12-12 06:53:27 +0100

    update question solution

--- question5/question5.py
+++ question5/question5.py
@@ -1,39 +1,56 @@
-import os, os.path
-import fnmatch
-#Number of .py file in folder quant_interview
-list = fnmatch.filter(os.listdir('.'), '*.py')
-print(list, len(list))
+import os
+import subprocess
+from os.path import dirname
 
-#Total umber of code lines
-count_code_line = 0
-for file_name in list:
-    file = open(file_name, 'r')
-    for line in file:
-        if line != '\n':
-            line_ = line.strip()
-            if not line_.startswith('#'):
-                count_code_line += 1
 
-print(count_code_line)
+def count_python_files(dir):
+    result = 0
+    for _, _, files in os.walk(dir):
+        result += len([f for f in files if f[-3:] == '.py'])
+    return result
 
-#Total number of comment lines
-count_cmt_line = 0
-for file_name in list:
-    file = open(file_name, 'r')
-    for line in file:
-            line_ = line.strip()
-            if line_.startswith('#'):
-                count_cmt_line += 1
 
-print(count_cmt_line)
+def count_line_in_python_file(py_txt_format):
+    count_total = 0
+    count_comment = 0
+    count_code = 0
+    count_function = 0
+    for line in py_txt_format:
+        if line == '':
+            continue
+        count_total += 1
+        if line.startswith('#'):
+            count_comment += 1
+        else:
+            count_code += 1
+        if line.startswith('def'):
+            count_function += 1
+    return count_total, count_comment, count_code, count_function
 
-#Number of definition
-count_definition = 0
-for file_name in list:
-    file = open(file_name, 'r')
-    for line in file:
-            line_ = line.strip()
-            if line_.startswith('def'):
-                count_definition += 1
 
-print(count_definition)
+def count_lines(dir):
+    n_total = 0
+    n_codes = 0
+    n_comments = 0
+    n_functions = 0
+    for roots, dirs, files in os.walk(dir):
+        for f in files:
+            if f.endswith('.py'):
+                print(os.path.join(roots, f))
+                with open(os.path.join(roots, f), 'r') as f_count:
+                    py_lines = f_count.read().split('\n')
+                    count_total, count_comment, count_code, count_function = count_line_in_python_file(py_lines)
+                    n_total += count_total
+                    n_codes += count_code
+                    n_comments += count_comment
+                    n_functions += count_function
+                    print("total: {}\tcodes: {}\t comments: {}\t functions:{}".format(n_total, n_codes, n_comments,
+                                                                                      n_functions))
+    return n_total, n_codes, n_comments, n_functions
+
+
+
+
+
+if __name__ == '__main__':
+    print(get_size('/home/dungdv/source/Quantitative_Interview'))
